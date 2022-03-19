5a03f3a08d9ba5dc8edebdcd5c79bdd0c4ae9c85
Koom <m.garnier.mickael@gmail.com>
2022-03-19 03:01:12 +0100

    Fixed unused import

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use asm_6502;
-
-fn main() {
+pub(crate) fn main() {
     println!("#### COMPILE SNAKE");
     let text = std::fs::read_to_string("src/script/snake.asm").unwrap();
     asm_6502::compile(text, 0x600);
